d0d9c0bcc9b529c220a87f90e749c8366e8315ae
Alex Kyungho Park <kyungho.dev@gmail.com>
2018-01-06 18:13:37 +0900

    #2 Model Coded
    - Model of Post app done
    - Not registered on Admin

--- blog/blogapp/admin.py
+++ blog/blogapp/admin.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
 
+from blogapp.models import Post
+
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'modify_date')
+    
--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -10,3 +10,28 @@ from django.core.urlresolvers import reverse
 
 class Post(models.Model):
     title = models.CharField('TITLE', max_length=50)
+    slug = models.SligField('SLUG', unique=True, allow_unicode=True, help_text="Do summary here")
+    description = models.CharField('DESCRIPTION', blank=True, help_text="description text")
+    content = models.TextField('TEXT')
+    create_date = models.DateTimeField('Create Date', auto_now_add=True)
+    modify_date = models.DateTimeField('Modify Date', auto_now=True)
+
+
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
+        db_table = 'my_post'
+        ordering = ('-modify_date',)
+    
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=(self.slug,))
+
+    def get_previous_post(self):
+        return self.get_previous_by_modify_date()
+    
+    def get_next_post(self):
+        return self.get_next_by_modify_date()
+    
