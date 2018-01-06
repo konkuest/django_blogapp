183a58536ca2f53ca4c1b74b0fa2d7e5f44311b1
Alex Kyungho Park <kyungho.dev@gmail.com>
2018-01-06 22:07:50 +0900

    #5 View
    View done

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -3,4 +3,41 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.views.generic import ListView, DetailView
+
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView
+from django.views.generic.dates import DayArchiveView, TodayArchiveView
+
+from blogapp.models import Post
+
 # Create your views here.
+
+class PostLV(ListView):
+    model = Post
+    template_name = 'blogapp/post_all.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+class PostDV(DetailView):
+    model = Post
+
+class PostAV(ArchiveIndexView):
+    model = Post
+    date_field = 'modify_date'
+    
+class PostYAV(YearArchiveView):
+    model = Post
+    date_field = 'modify_date'
+    make_object_list = True
+
+class PostMAV(MonthArchiveView):
+    model = Post
+    date_field = 'modify_date'
+
+class PostDAV(DayArchiveView):
+    model = Post
+    date_field = 'modify_date'
+
+class PostTAV(TodayArchiveView):
+    model = Post
+    date_field = 'modify_date'
